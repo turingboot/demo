caa2fd3a997f4442fe9ad78e4f160066ae24efa1
turingboot <turingboot@users.noreply.github.com>
2022-11-25 00:06:49 +0800

    commit test

--- src/basic/App.js
+++ src/basic/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     render(){
         return(
             <div>
-                <h2>你好世界</h2>
+                <h2>你好世界!!!</h2>
                 <input type="text"></input>
             </div>
         )
