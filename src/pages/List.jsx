95909cff27d57a774eb072ba78155e1d2cd6a533
turingboot <turingboot@users.noreply.github.com>
2022-11-26 03:16:45 +0800

    router

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 export default function List() {
-  
+
 
   // /list/123
   // useParams()获取123
-  const {id} = useParams()
+  const { id } = useParams()
   console.log(id)
   return (
     <div>List</div>
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,21 +5,21 @@ import Home from '../pages/Home'
 import Detail from '../pages/Detail'
 import List from '../pages/List'
 import Error from '../pages/Error'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 //定义路由
 const BaseRouter = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App></App>}>
-                <Route path="/home" element={<Home/>}></Route>
-                <Route path="/detail" element={<Detail/>}></Route>
-                <Route path="/list/:id" element={<List/>}></Route>
+                <Route path="/home" element={<Home />}></Route>
+                <Route path="/detail" element={<Detail />}></Route>
+                <Route path="/list/:id" element={<List />}></Route>
             </Route>
             {/* 404页面的处理 */}
-            <Route path="*" element={<Error/>}></Route>
+            <Route path="*" element={<Error />}></Route>
         </Routes>
-        
+
     </BrowserRouter>
 )
 
